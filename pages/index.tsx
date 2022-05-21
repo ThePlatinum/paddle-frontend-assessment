609c79e72d8bd52dad4ae7ece328fc9d9ae43203
Platinum <platinumemirate@gmail.com>
2022-05-21 22:40:37 +0100

    header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import s from '../styles/Home.module.css'
 
 export default function Home() {
@@ -11,8 +12,21 @@ export default function Home() {
       </Head>
 
       <main className="main container">
-        <header className={s.header}>
-          Header
+        <header className={`${s.header} d-flex py-3`}>
+          <img src="/favicon.ico" alt="logo" width={60} height={60} />
+          <div className={`nav ${s.nav}`}>
+            <ul>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li className={`${s.btn}`}>
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+          </div>
         </header>
         <div className={s.body}>
         main body
