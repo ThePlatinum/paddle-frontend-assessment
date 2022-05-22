c0cd42ae3076d8b985948ad7e8ea84670023d038
Platinum <platinumemirate@gmail.com>
2022-05-22 10:21:46 +0100

    body

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
 import Head from 'next/head'
+import { useState } from 'react'
 import s from '../styles/Home.module.css'
 
 export default function Home() {
+
+  const [Days, setDays] = useState(7)
+  const [Hours, setHours] = useState(24)
+  const [Minutes, setMinutes] = useState(0)
+  const [Seconds, setSeconds] = useState(0)
+
+  const times = [
+    {
+      time : Days,
+      text : "Days"
+    },
+    {
+      time : Hours,
+      text : "Hours"
+    },
+    {
+      time : Minutes,
+      text : "Minutes"
+    },
+    {
+      time : Seconds,
+      text : "Seconds"
+    },
+  ]
+
   return (
     <div>
       <Head>
@@ -12,7 +38,7 @@ export default function Home() {
 
       <main className={`${s.main} main container`}>
         <header className={`${s.header} d-flex py-3`}>
-          <img src="/favicon.ico" alt="logo" width={60} height={60} />
+          <img src="/favicon.ico" alt="logo" width={50} height={50} />
           <div className={`nav ${s.nav}`}>
             <ul>
               <li>
@@ -27,9 +53,43 @@ export default function Home() {
             </ul>
           </div>
         </header>
+
         <div className={s.body}>
-        main body
+          
+          <h1>
+            SOMETHING AWESOME IS <br /> COMING SOON
+          </h1>
+
+          <p>
+            Your all-in-one affliate marketing tracking software <b>tract</b>, 
+            <b> automate</b> and <b>optimize</b> your campaigns.
+          </p>
+
+          <div className={`${s.timer} d-flex`}>
+            {
+              times.map(t=>{
+                return(
+                  <div className={`${s.timer__item} card`}>
+                    <h3> {t.time} </h3>
+                    <p> {t.text} </p>
+                  </div>
+                )
+              })
+            }
+          </div>
+
+          <div className={`${s.name} d-flex`}>
+            <input type="text" name="firstname" id="firstname" placeholder='First Name.' />
+            <input type="text" name="lastname" id="lastname" placeholder='Last Name.' />
+          </div>
+
+          <div className={`${s.email} d-flex`}>
+            <input type="email" placeholder='Enter your email address'/>
+            <input type="button" value="JOIN OUR WAITING LIST" className={`${s.btn}`} />
+          </div>
+          
         </div>
+
         <footer className={`${s.footer} d-flex flex-column`}>
           <div className="socials">
             <a href="#"> <i className='bx bxl-youtube'></i> </a>
